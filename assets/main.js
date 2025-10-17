b6eddc000fe5c5e7ea4d744627a43e507b2079a2
binhan012 <binhanvio@gmail.com>
2025-10-17 10:17:48 +0700

    fix

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,11 @@
-(
-    function() {
-        const backGround = document.querySelector('.section-1');
-        let images = ['banner1.jpg', 'banner2.jpg'];
-        let index = 0;
-        setInterval(() => {
-            index = (index + 1) % images.length;
-            backGround.style.backgroundImage = `url(assets/img/${images[index]})`;
-        },7000);
-    }
-)()
+(function() {
+    const img = document.getElementById("img-section-1");
+    let images = ['banner1.jpg', 'banner2.jpg', 'banner3.jpg'];
+    const imagePath = 'assets/img/'; 
+
+    let index = 0;
+    setInterval(() => {
+        index = (index + 1) % images.length; 
+        img.src = imagePath + images[index];    
+    }, 3000);
+})();
